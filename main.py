20d7368c317fd8c5e903d53c4335ed20bc0441f4
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2022-12-12 15:29:43 +0100

    count bombs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import numpy as np
+from PIL import ImageTk, Image
 
 class App:
     def __init__(self, width: int, height: int, bombs: int):
@@ -32,30 +33,63 @@ class App:
         self.game_started = False
         self.bombs = bombs
 
+        self.load_images()
+        
+    def load_images(self):
+        img = Image.open("tiles.png")
+        tiles = []
+        img_width = img.width/4
+        img_height = img.height/3
+        for r in range(3):
+            for c in range(4):
+                tiles.append(img.crop((img_width*c, img_height*r, img_width*(c+1), img_height*(r+1))))
+        self.tiles = {
+            "": tiles[0], "safe": tiles[1], "-1": tiles[2], "0": tiles[3],
+            "1": tiles[4], "2": tiles[5], "3": tiles[6], "4": tiles[7],
+            "5": tiles[8], "6": tiles[9], "7": tiles[10], "8": tiles[11]
+        }
+
     def place_bombs(self, r, c):
         safe_cells = 9
         if (r in [0, self.height-1] and c in [0, self.width-1]): 
             safe_cells -= 5
         if (r == 0 or r == self.height-1) ^ (c == 0 or c == self.width-1): 
             safe_cells -= 3
-        print(safe_cells)
         p = 1 / (self.width*self.height - safe_cells)
         mask = np.full((self.height,self.width), p)
         # Using slice set surrunding cells as zero
         mask[max(r-1,0) : min(r+2,self.height)  ,  max(c-1,0) : min(c+2,self.width)] = 0
-        # Flattened indices
+        # Flattened indices place bombs using numpy choice. Uniform distribution along every index
         bomb_indices = np.random.choice(self.width*self.height, self.bombs, replace=False, p = mask.flatten())
+        # Create flattened board
         self.board = np.zeros((self.height*self.width))
+        # Bomb indices get value of -1
         self.board[bomb_indices] = -1
-        self.board.reshape((self.height, self.width))
+        # Turn the very long array into a matrix
+        self.board = self.board.reshape((self.height, self.width))
+
+    def count_bombs(self):
+        for r in range(self.height):
+            for c in range(self.width):
+                if self.board[r,c] == -1: continue
+                self.board[r,c] = list(self.get_surrounding_cells(r, c)).count(-1)
+
+    def get_surrounding_cells(self, row, column):
+        for r in range(max(row-1, 0), min(row+2, self.height)):
+            for c in range(max(column-1, 0), min(column+2, self.width)):
+                yield self.board[r,c]
         
     def reveal(self, event: tk.Event):
         info = event.widget.grid_info()
         row, column = info["row"], info["column"]
-        if self.cells[row][column][1] == "":
-            self.cells[row][column][1] = "opened"
-            event.widget.configure(bg="red")
+        if not self.game_started:
             self.place_bombs(row, column)
+            self.count_bombs()
+        if self.cells[row][column][1] == "":
+            label = str(int(self.board[row,column]))
+            self.cells[row][column][1] = label
+            tk_label = tk.Label(event.widget, image=ImageTk.PhotoImage(self.tiles[label]))
+            tk_label.pack()
 
     def mark(self, event: tk.Event):
         info = event.widget.grid_info()
