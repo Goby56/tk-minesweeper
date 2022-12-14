e451e656b9201dbd0da1693689948bbc13aa0f45
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2022-12-14 10:57:04 +0100

    Update tile images

--- main.py
+++ main.py
@@ -11,31 +11,35 @@ class App:
         self.root.geometry(f"{self.width*25}x{self.height*25}")
         self.root.minsize(self.width*15, self.height*15)
 
+        self.load_tile_images()
+
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=1)
 
         self.frame = tk.Frame(self.root)
         self.frame.grid(row=0, column=0, sticky="news")
+        self.frame.bind("<Configure>", self.on_window_resize)
 
         self.cells = [[None for j in range(self.width)] for i in range(self.height)]
 
         for r in range(self.height):
             for c in range(self.width):
-                button = tk.Frame(self.frame, bg="white", relief="raised", highlightthickness=2)
-                button.grid(row=r, column=c, sticky="news")
-                button.bind("<Button-1>", self.reveal)
-                button.bind("<Button-3>", self.mark)
-                self.cells[r][c] = [button, ""]
+                tk_frame = tk.Frame(self.frame)
+                tk_frame.grid(row=r, column=c, sticky="news")
+                tk_frame.bind("<Button-1>", self.reveal)
+                tk_frame.bind("<Button-3>", self.mark)
+                tk_label = tk.Label(tk_frame)
+                tk_label.pack()
+                self.cells[r][c] = [tk_frame, ""]
+                self.update_tile_image(r, c)
             
         self.frame.columnconfigure(tuple(range(self.width)), weight=1)
         self.frame.rowconfigure(tuple(range(self.height)), weight=1)
 
         self.game_started = False
         self.bombs = bombs
-
-        self.load_images()
         
-    def load_images(self):
+    def load_tile_images(self):
         img = Image.open("tiles.png")
         tiles = []
         img_width = img.width/4
@@ -43,11 +47,29 @@ class App:
         for r in range(3):
             for c in range(4):
                 tiles.append(img.crop((img_width*c, img_height*r, img_width*(c+1), img_height*(r+1))))
-        self.tiles = {
+        self.tile_images = {
             "": tiles[0], "safe": tiles[1], "-1": tiles[2], "0": tiles[3],
             "1": tiles[4], "2": tiles[5], "3": tiles[6], "4": tiles[7],
             "5": tiles[8], "6": tiles[9], "7": tiles[10], "8": tiles[11]
         }
+
+    def update_tile_image(self, r, c):
+        tk_frame = self.cells[r][c][0]
+        label = self.cells[r][c][1]
+        # tile_size = (tk_frame.winfo_width(), tk_frame.winfo_height())
+        tile_size = (25,25)
+        img = self.tile_images[label].resize(tile_size)
+        photo_img = ImageTk.PhotoImage(img)
+        tk_label = tk_frame.winfo_children()[0]
+        tk_label.configure(image=photo_img)
+        tk_label.image = photo_img
+
+    def on_window_resize(self, event: tk.Event):
+        # tile_size = (event.width // self.width, event.height // event.width)
+        print("resize")
+        for r in range(self.height):
+            for c in range(self.width):
+                self.update_tile_image(r, c)
 
     def place_bombs(self, r, c):
         safe_cells = 9
@@ -78,7 +100,7 @@ class App:
         for r in range(max(row-1, 0), min(row+2, self.height)):
             for c in range(max(column-1, 0), min(column+2, self.width)):
                 yield self.board[r,c]
-        
+
     def reveal(self, event: tk.Event):
         info = event.widget.grid_info()
         row, column = info["row"], info["column"]
@@ -88,8 +110,7 @@ class App:
         if self.cells[row][column][1] == "":
             label = str(int(self.board[row,column]))
             self.cells[row][column][1] = label
-            tk_label = tk.Label(event.widget, image=ImageTk.PhotoImage(self.tiles[label]))
-            tk_label.pack()
+            self.update_tile_image(row, column)
 
     def mark(self, event: tk.Event):
         info = event.widget.grid_info()
