79bb9394e01f2232636b7d4a36d31b15644a2850
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2023-01-10 15:45:08 +0100

    Dynamic widgets

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ class Menu:
 
         self.rows = self.config["default_grid"]["y"]
         self.columns = self.config["default_grid"]["x"]
-        self.bomb_amount = self.config["default_bomb_amount"]
+        self.bombs = self.config["default_bomb_amount"]
 
         width = self.columns * self.cell_size
         height = (self.rows+2)*self.cell_size
@@ -306,43 +306,69 @@ class Menu:
         photo_img = ImageTk.PhotoImage(self.images["start"].resize((self.cell_size*5, self.cell_size*2)))
         self.start_game_button.configure(image=photo_img)
         self.start_game_button.image = photo_img
-        self.start_game_button.bind("<Button-1>", lambda event: self.switch_frame(dimensions=(self.rows, self.columns), number_of_bombs=self.bomb_amount))
-
+        self.start_game_button.bind("<Button-1>", lambda event: self.switch_frame(dimensions=(self.rows, self.columns), number_of_bombs=self.bombs))
+        
         self.row_select = tk.Label(self.frame, relief=tk.RAISED, borderwidth=10)
         self.row_select.grid(row=2, column=1, padx=self.cell_size*0.25)
-        photo_img = ImageTk.PhotoImage(self.images["row_select"].resize((self.cell_size*3, int(self.cell_size*1.5))))
-        self.row_select.configure(image=photo_img)
-        self.row_select.image = photo_img
+        # photo_img = ImageTk.PhotoImage(self.images["row_select"].resize((self.cell_size*3, int(self.cell_size*1.5))))
+        # self.row_select.configure(image=photo_img)
+        # self.row_select.image = photo_img
         self.row_select.bind("<MouseWheel>", lambda event: self.on_widget_scrolled(widget="rows", event=event))
 
         self.column_select = tk.Label(self.frame, relief=tk.RAISED, borderwidth=10)
         self.column_select.grid(row=2, column=0, padx=self.cell_size*0.25)
-        photo_img = ImageTk.PhotoImage(self.images["row_select"].resize((self.cell_size*3, int(self.cell_size*1.5))))
-        self.column_select.configure(image=photo_img)
-        self.column_select.image = photo_img
+        # photo_img = ImageTk.PhotoImage(self.images["row_select"].resize((self.cell_size*3, int(self.cell_size*1.5))))
+        # self.column_select.configure(image=photo_img)
+        # self.column_select.image = photo_img
         self.column_select.bind("<MouseWheel>", lambda event: self.on_widget_scrolled(widget="columns", event=event))
         
         self.bomb_amount_select = tk.Label(self.frame, relief=tk.RAISED, borderwidth=10)
         self.bomb_amount_select.grid(row=2, column=2, padx=self.cell_size*0.25)
-        photo_img = ImageTk.PhotoImage(self.images["row_select"].resize((self.cell_size*3, int(self.cell_size*1.5))))
-        self.bomb_amount_select.configure(image=photo_img)
-        self.bomb_amount_select.image = photo_img
-        self.bomb_amount_select.bind("<MouseWheel>", lambda event: self.on_widget_scrolled(widget="bomb_amount", event=event))
+        # photo_img = ImageTk.PhotoImage(self.images["row_select"].resize((self.cell_size*3, int(self.cell_size*1.5))))
+        # self.bomb_amount_select.configure(image=photo_img)
+        # self.bomb_amount_select.image = photo_img
+        self.bomb_amount_select.bind("<MouseWheel>", lambda event: self.on_widget_scrolled(widget="bombs", event=event))
 
         self.frame.grid_columnconfigure((0, 1, 2), weight=1)
+
+        self.dynamic_widgets = {"rows": self.row_select, "columns": self.column_select, "bombs": self.bomb_amount_select}
+
+        self.update_image(widget="rows")
+        self.update_image(widget="columns")
+        self.update_image(widget="bombs")
 
     def on_widget_scrolled(self, widget: str, event: tk.Event):
         value = getattr(self, widget)
         setattr(self, widget, max(4, min(value + event.delta // 120, 99)))
     
-        print("rows:", self.rows, " columns:", self.columns, " bombs:", self.bomb_amount)
+        print("rows:", self.rows, " columns:", self.columns, " bombs:", self.bombs)
 
-    def update_images(self):
-        pass
+    def update_image(self, widget: str):
+        value = str(getattr(self, widget))
+        if len(value) == 1:
+            value = "0" + value
+        
+        label_img = self.images[widget].resize((self.cell_size, int(self.cell_size*1.5)))
+        tens_img = self.images[value[0]].resize((self.cell_size, int(self.cell_size*1.5)))
+        ones_img = self.images[value[1]].resize((self.cell_size, int(self.cell_size*1.5)))
+
+        target_width = self.cell_size * 3
+        target_height = int(self.cell_size * 1.5)
+        img = Image.new("RGB", (target_width, target_height))
+        img.paste(label_img, (0, 0))
+        img.paste(tens_img, (self.cell_size * 3, 0))
+        img.paste(ones_img, (self.cell_size * 6, 0))
+
+        photo_img = ImageTk.PhotoImage(img)
+        self.dynamic_widgets[widget].configure(image=photo_img)
+        self.dynamic_widgets[widget].image = photo_img
 
     def load_images(self):
         self.images = {
-            "start": Image.open("start.png"), "row_select": Image.open("row_select.png")
+            "start": Image.open("start.png"), "columns": Image.open("x.png"), "rows": Image.open("y.png"), "bombs": Image.open("b.png"),
+            "0": Image.open("0.png"), "1": Image.open("1.png"), "2": Image.open("2.png"), "3": Image.open("3.png"),
+            "4": Image.open("4.png"), "5": Image.open("5.png"), "6": Image.open("6.png"), "7": Image.open("7.png"),
+            "8": Image.open("8.png"), "9": Image.open("9.png"), "row_select": Image.open("row_select.png")
         }
         
 
